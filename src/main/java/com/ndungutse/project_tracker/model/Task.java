b46a823ead04c06bd38f794a515361a6939d0cb8
ericndungutse <dav.ndungutse@gmail.com>
2025-06-08 14:34:06 +0200

    feat: create task model

---
+++ src/main/java/com/ndungutse/project_tracker/model/Task.java
@@ -0,0 +1,75 @@
+package com.ndungutse.project_tracker.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean status;
+    private LocalDate dueDate;
+
+    // Default constructor
+    public Task() {
+    }
+
+    // Constructor with parameters
+    public Task(
+            String title,
+            String description,
+            boolean status,
+            LocalDate dueDate
+    ) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
